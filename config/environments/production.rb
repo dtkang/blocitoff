2b574cf62778fb080451163a82a920577f621e84
Dan Kang <dtkang@gmail.com>
2016-01-18 22:33:15 +0000

    Update devise configuration parameters

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'blocitoff.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'fast-waters-7512.herokuapp.com' }
   config.assets.initialize_on_precompile = false
 end
