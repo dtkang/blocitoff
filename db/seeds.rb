cc1ac322fcacf2197d55dddb00b74dc39d2e8327
Dan Kang <dtkang@gmail.com>
2016-02-01 02:28:10 +0000

    Completed lesson 6

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,8 @@
 
-User.create( :email => "admin@bloccitoff.com", :password => "helloworld", :password_confirmation => "helloworld", :confirmed_at => Time.now )
+user = User.create( :email => "admin@example.com", :password => "helloworld", :password_confirmation => "helloworld", :confirmed_at => Time.now )
+7.times {
+  user.items.create( :name => Faker::Hacker.say_something_smart )
+}
 
-p "admin@bloccitoff.com user created!"
+p "admin@example.com user created!"
+p user.items.count.to_s + " to do items created for admin"
