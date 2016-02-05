6515bb6b0138be265193e1ec4f4969ee10befaa8
Dan Kang <dtkang@gmail.com>
2016-02-05 02:51:12 +0000

    Completed destroy items capability, more ajax functionality

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   def create
     @user = User.find(params[:user_id])
     @item = @user.items.new(item_params)
+    @new_item = Item.new
     
     if @item.save
       flash[:notice] = "Item created"
@@ -14,6 +15,22 @@ class ItemsController < ApplicationController
       f.js
     end
   end
+  
+  def destroy
+    @user = User.find(params[:user_id])
+    @item = @user.items.find(params[:id])
+    
+    if @item.delete
+      flash[:notice] = "Item deleted"
+    else
+      flash[:error] = "Failed to delete item"
+    end
+    
+    respond_to do |f|
+      f.html
+      f.js
+    end
+  end  
   
   private
   
